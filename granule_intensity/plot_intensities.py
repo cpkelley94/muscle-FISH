fb458148f4df981d7c7faaa5772a59bac4827f9b
cpkelley94 <cpkelley94@ufl.edu>
2021-02-20 14:41:29 -0500

    update simulation readme

--- granule_intensity/plot_intensities.py
+++ granule_intensity/plot_intensities.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from copy import deepcopy
 from matplotlib import pyplot as plt
 import argparse
 import csv
@@ -7,77 +8,194 @@ import os
 import pandas as pd
 import seaborn as sns
 
+def calculate_ci(real, bootstraps, ci=0.95):
+    real_sorted = sorted(real)
+    bs_sorted = [sorted(b) for b in bootstraps]
+    bs_y = [[1.-(float(j)/len(b)) for j in range(len(b))] for b in bs_sorted]
+    y_mins = []
+    y_maxs = []
+
+    for x in real_sorted:
+        ys = [np.interp(x, b, bs_y[i]) for i, b in enumerate(bs_sorted)]
+        y_min = np.percentile(ys, 100*(1.-ci)/2.)
+        y_max = np.percentile(ys, 100*(1.+ci)/2.)
+        y_mins.append(y_min)
+        y_maxs.append(y_max)
+    
+    return y_mins, y_maxs
+
+
 # define arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('infolder', type=str, nargs=1, help='Path to folder containing merged 3D Gaussian curve fit data.')
-parser.add_argument('outfolder', type=str, nargs=1, help='Path to output directory.')
+parser.add_argument('folders', type=str, nargs='*', help='Paths to folder containing merged spot intensity data.')
+parser.add_argument('-c', '--conditions', nargs='*', help='Names of conditions.', default=None)
+parser.add_argument('-o', '--outfolder', help='Path to output directory.', default=os.getcwd())
 
 # parse arguments
 args = vars(parser.parse_args())
-in_dir = args['infolder'][0]
-out_dir = args['outfolder'][0]
+in_dirs = args['folders']
+conditions = args['conditions']
+out_dir = args['outfolder']
 
-files = [f for f in os.listdir(in_dir) if f.startswith('intens_merged') and f.endswith('.csv')]
-spots_dict = defaultdict(dict)
+# check user input
+if not conditions:
+    conditions = [str(i+1) for i in range(len(in_dirs))]
+elif len(conditions) != len(in_dirs):
+    conditions = [str(i+1) for i in range(len(in_dirs))]
+
+# iterate through conditions and get all gene names
+spots_dict = {}
+large_intens = {}
 genes = set()
+for c, in_dir in zip(conditions, in_dirs):
+    for f in os.listdir(in_dir):
+        if f.startswith('intens_merged') and f.endswith('.csv'):
+            gene = f.split('^')[1].split('.')[0]
+            genes.add(gene)
 
-for f in files:
-    print(f)
-    gene = f.split('^')[1].split('.')[0]
-    if '_cyt^' in f:
-        outname = 'hist_intens_cyt^' + gene + '.pdf'
-        category = 'cyt'
-    elif '_peri^' in f:
-        outname = 'hist_intens_peri^' + gene + '.pdf'
-        category = 'peri'
+# create empty entries
+for gene in sorted(list(genes)):
+    spots_dict[gene] = {c:{'cyt':[], 'peri':[]} for c in conditions}
+    large_intens[gene] = {c:{'cyt':[], 'peri':[]} for c in conditions}
 
-    # open spots file
-    with open(os.path.join(in_dir, f), 'r') as infile:
-        reader = csv.reader(infile)
-        lines = [row for row in reader]
+# iterate over file data
+for c, in_dir in zip(conditions, in_dirs):
+    files = [f for f in os.listdir(in_dir) if f.startswith('intens_merged') and f.endswith('.csv')]
 
-    intensities = [float(val) for val in lines[0]]
-    spots_dict[gene][category] = intensities
-    genes.add(gene)
+    for f in files:
+        print(f)
+        gene = f.split('^')[1].split('.')[0]
+        if '_cyt^' in f:
+            category = 'cyt'
+        elif '_peri^' in f:
+            category = 'peri'
 
-    # plot spot intensity histograms
+        # open spots file
+        with open(os.path.join(in_dir, f), 'r') as infile:
+            reader = csv.reader(infile)
+            lines = [row for row in reader]
+
+        intensities = [float(val) for val in lines[0]]
+        spots_dict[gene][c][category] = deepcopy(intensities)
+        large_intens[gene][c][category] = np.percentile(intensities, 95)
+
+# make CDF plots
+print('\nDrawing CDF plots...')
+for g in sorted(list(genes)):
     fig, ax = plt.subplots()
-    ax.hist(intensities, bins=200, range=(0, 200))
-    ax.set_xlim([0, 200])
-    ax.set_xlabel('Spot intensity')
-    ax.set_ylabel('Frequency')
-    plt.savefig(os.path.join(out_dir, outname), dpi=300)
-    plt.close()
+    x_C = sorted(spots_dict[g]['C']['peri'])
+    y_C = [float(i)/len(x_C) for i in range(len(x_C))]
+    x_N = sorted(spots_dict[g]['N']['peri'])
+    y_N = [float(i)/len(x_N) for i in range(len(x_N))]
 
-    fig, ax = plt.subplots()
-    ax.hist(intensities, bins=np.logspace(-0.5,4,100))
+    if x_C == [] or x_N == []:
+        continue
+
+    ax.step(x_C, y_C, 'k')
+    ax.step(x_N, y_N, 'r')
     ax.set_xscale('log')
+
+    # ax.hist(spots_dict[g]['C']['peri'], normed=True, cumulative=True, label='control', histtype='step', color='#aaaaaa')
+    # ax.hist(spots_dict[g]['N']['peri'], normed=True, cumulative=True, label='control', histtype='step', color='r')
+    ax.set_ylabel('CDF')
     ax.set_xlabel('Spot intensity')
-    ax.set_ylabel('Frequency')
-    plt.savefig(os.path.join(out_dir, 'log_' + outname), dpi=300)
+    plt.tight_layout()
+    plt.savefig(os.path.join(out_dir, 'cdf_intens^' + g + '.pdf'), dpi=300)
     plt.close()
 
 # make swarm plots
+print('Drawing strip plots...')
 for g in sorted(list(genes)):
-    print(g)
+    # print(g)
     data_list = []
     for cat in ['cyt', 'peri']:
-        for intens in spots_dict[g][cat]:
-            data_list.append([cat, intens])
+        for c in conditions:
+            for intens in spots_dict[g][c][cat]:
+                data_list.append([c+'_'+cat, intens])
     
     df = pd.DataFrame(data_list, columns=['compartment', 'intensity'])
 
     # make strip plots
-    ax = sns.stripplot(x='compartment', y='intensity', data=df, size=2)
+    ax = sns.stripplot(x='compartment', y='intensity', data=df, size=2, jitter=0.4, color='#aaaaaa')
     ax.set_ylabel('Spot intensity')
     plt.tight_layout()
-    plt.savefig(os.path.join(out_dir, 'strip_intens^' + g + '.pdf'), dpi=300)
+    plt.savefig(os.path.join(out_dir, 'combined_strip_intens^' + g + '.pdf'), dpi=300)
     plt.close()
 
     fig, log_ax = plt.subplots()
     log_ax.set_yscale('log')  # log first
     log_ax.set_ylabel('Spot intensity')
-    sns.stripplot(x='compartment', y='intensity', data=df, ax=log_ax, size=2)
+    sns.boxplot(x='compartment', y='intensity', data=df, ax=log_ax, color='#bbbbbb', whis=np.inf, notch=True)
+    sns.stripplot(x='compartment', y='intensity', data=df, ax=log_ax, size=2, jitter=0.3, color='#5a5a5a')
+    
+    x = 0
+    for i, cat in enumerate(['cyt', 'peri']):
+        for j, c in enumerate(conditions):
+            y = large_intens[g][c][cat]
+            if y:
+                log_ax.plot([x-0.4, x+0.4], [y, y], 'r-')
+                x += 1
+
     plt.tight_layout()
-    plt.savefig(os.path.join(out_dir, 'strip_log_intens^' + g + '.pdf'), dpi=300)
+    plt.savefig(os.path.join(out_dir, 'combined_strip_log_intens^' + g + '.pdf'), dpi=300)
+    plt.close()
+
+# make CCDF plots
+print('Drawing tail distribution plots (with bootstrapping)...')
+for g in sorted(list(genes)):
+    if g in ['ACTB', 'ACTN2', 'ATP2A1', 'CHRNE']:
+        continue
+    
+    print('\n'+g)
+    fig, ax = plt.subplots()
+    x_C = sorted(spots_dict[g]['C']['peri'])
+    y_C = [1.-(float(i)/len(x_C)) for i in range(len(x_C))]
+    x_N = sorted(spots_dict[g]['N']['peri'])
+    y_N = [1.-(float(i)/len(x_N)) for i in range(len(x_N))]
+
+    if x_C == [] or x_N == []:
+        continue
+
+    # bootstrap
+    bs_x_C = []
+    bs_y_C = []
+    bs_x_N = []
+    bs_y_N = []
+
+    n_samples = 100
+    
+    print('Bootstrapping...')
+    for n in range(n_samples):
+        sample_C = sorted(np.random.choice(spots_dict[g]['C']['peri'], size=len(spots_dict[g]['C']['peri'])))
+        sample_N = sorted(np.random.choice(spots_dict[g]['N']['peri'], size=len(spots_dict[g]['N']['peri'])))
+        bs_x_C.append(sample_C)
+        bs_y_C.append([1.-(float(i)/len(sample_C)) for i in range(len(sample_C))])
+        bs_x_N.append(sample_N)
+        bs_y_N.append([1.-(float(i)/len(sample_N)) for i in range(len(sample_N))])
+    
+    print('Calculating CIs...')
+    y_mins_C, y_maxs_C = calculate_ci(x_C, bs_x_C, ci=0.95)
+    y_mins_N, y_maxs_N = calculate_ci(x_N, bs_x_N, ci=0.95)
+
+    # plot
+    print('Drawing plot...')
+    ax.step(x_C, y_C, 'k')
+    ax.step(x_N, y_N, 'r')
+    ax.fill_between(x_C, y_mins_C, y_maxs_C, step='pre', color='k', alpha=0.3, linewidth=0)
+    ax.fill_between(x_N, y_mins_N, y_maxs_N, step='pre', color='r', alpha=0.3, linewidth=0)
+
+
+    # for n in range(n_samples):
+    #     ax.step(bs_x_C[n], bs_y_C[n], 'k', alpha=0.2)
+    #     ax.step(bs_x_N[n], bs_y_N[n], 'r', alpha=0.2)
+
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+
+    # ax.hist(spots_dict[g]['C']['peri'], normed=True, cumulative=True, label='control', histtype='step', color='#aaaaaa')
+    # ax.hist(spots_dict[g]['N']['peri'], normed=True, cumulative=True, label='control', histtype='step', color='r')
+    ax.set_ylabel('Tail distribution')
+    ax.set_xlabel('Spot intensity')
+    plt.tight_layout()
+    plt.savefig(os.path.join(out_dir, 'taildist_intens^' + g + '.pdf'), dpi=300)
     plt.close()
---
+++ rna_decay/estimate_decay.py
@@ -0,0 +1,262 @@
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.size'] = 6
+rcParams['font.sans-serif'] = ['Arial']
+
+from collections import defaultdict
+from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.stats import pearsonr, linregress
+import argparse
+import csv
+import numpy as np
+
+def expo(x, A, b):
+    return A*np.exp(-1.*b*x)  # fit function for scipy.optimize.curve_fit()
+
+# define arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('tables', type=str, nargs='*',
+    help='Paths to the cytoplasmic RNA density tables.')
+parser.add_argument('-t', '--times', type=int, nargs='*',
+    help='Time points for each density table.', default=None)
+parser.add_argument('-n', '--noco', help='Path to nocodazole depletion .CSV file.', default=None)
+parser.add_argument('-d', '--densities', help='Path to cytoplasmic densities from nocodazole experiment.', default=None)
+
+# parse arguments
+args = vars(parser.parse_args())
+p_tables = args['tables']
+times = args['times']
+p_noco = args['noco']
+p_dens = args['densities']
+
+if times is None:
+    raise ValueError('No time points provided. Use \'t\' to provide a list of time points.')
+
+gene_names = ['Hnrnpa2b1', 'Atp2a1', 'Polr2a', 'Myom1',
+    'Gapdh', 'Hist1h1c', 'Vcl', 'Chrne1',
+    'Ttn', 'Dmd', 'Actn2']
+
+# open the tables
+data_by_time = []
+for j, p in enumerate(p_tables):
+    with open(p, 'r') as table_file:
+        reader = csv.reader(table_file)
+        data = {gene_names[int(row[0])-1]:np.array(row[1:]).astype(float) for i, row in enumerate(reader) if i}
+        data_by_time.append(data)
+# print(data_by_time)
+
+# reorganize
+mean_by_gene = defaultdict(list)
+err_by_gene = defaultdict(list)
+genes = set()
+for j in range(len(times)):
+    for g in data_by_time[j].keys():
+        genes.add(g)
+        mean_by_gene[g].append(np.mean(data_by_time[j][g]))
+        err_by_gene[g].append(np.std(data_by_time[j][g]))
+
+gene_list = sorted(list(genes))
+# print(gene_list)
+
+# curve_fit and make plots per gene
+genes_to_draw = ['Polr2a', 'Vcl', 'Dmd', 'Hist1h1c', 
+    'Hnrnpa2b1', 'Myom1', 'Ttn', 'Gapdh']
+curves = []
+curve_dict = {}
+fig, ax = plt.subplots(2, 4)
+fig.set_size_inches(8, 4.5)
+for i, g in enumerate(genes_to_draw):
+    # print(g)
+    #exp_params, exp_cov = curve_fit(expo, times, mean_by_gene[g], p0=[mean_by_gene[g][0], -np.log(2)/6.], sigma=err_by_gene[g], absolute_sigma=True)
+    exp_params, exp_cov = curve_fit(expo, times, mean_by_gene[g], p0=[mean_by_gene[g][0], -np.log(2)/6.])
+    curves.append([g, genes_to_draw[i]] + list(exp_params) + list(np.sqrt(np.diag(exp_cov))) + [np.log(2)/exp_params[1]])
+    curve_dict.update({genes_to_draw[i]:exp_params})
+    x_fit = np.linspace(min(times)-3, max(times)+3, num=250)
+    y_fit = expo(x_fit, *exp_params)
+
+    ij = (int(i/4), i%4)
+
+    ax[ij].errorbar(times, mean_by_gene[g], fmt='ko', yerr=err_by_gene[g], elinewidth=0.8, capsize=5)
+    ax[ij].plot(x_fit, y_fit, 'k-', lw=0.8)
+    ax[ij].set_xlim([min(times)-2, max(times)+2])
+    ax[ij].set_ylim(bottom=0)
+    # ax[ij].set_xlabel('Time (hr)')
+    # ax[ij].set_ylabel(r'Cytoplasmic RNA density $(\mathrm{spots} \; \mathrm{\mu m}^{-3})$')
+    ax[ij].set_title(g)
+    ax[ij].ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+
+    # Hide the right and top spines
+    ax[ij].spines['right'].set_visible(False)
+    ax[ij].spines['top'].set_visible(False)
+
+    # Only show ticks on the left and bottom spines
+    ax[ij].yaxis.set_ticks_position('left')
+    ax[ij].xaxis.set_ticks_position('bottom')
+
+plt.tight_layout()
+# plt.savefig('rna_decay_by_gene.png', dpi=300)
+plt.savefig('rna_decay_by_gene.pdf', dpi=300)
+plt.close()
+
+# output curves
+with open('exponential_decay_fit.csv', 'w') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerows([['id', 'gene', 'A (molec um^-3)', 'k (hr^-1)', 'dA (molec um^-3)', 'dk (hr^-1)', 'half-life (hr)']] + curves)
+
+# compare to nocodazole depletion data
+if p_noco is not None:
+    with open(p_noco, 'r') as noco_file:
+        reader = csv.reader(noco_file)
+        noco_data = {row[0].capitalize():float(row[1]) for row in reader}
+    
+    half_lives = []
+    fracs_theo = []
+    fracs_expt = []
+    genes_in_both = []
+
+    for name in gene_names:
+        if name in noco_data and name != 'Actn2':
+            frac_theo = expo(18., 1., curve_dict[name][1])
+            half_lives.append(np.log(2)/curve_dict[name][1])
+            fracs_theo.append(frac_theo)
+            fracs_expt.append(noco_data[name])
+            genes_in_both.append(name)
+    
+    # plot depletion vs. half-life
+    fig, ax = plt.subplots()
+    fig.set_size_inches(4,4)
+    ax.plot(half_lives, fracs_expt, 'k.')
+    # ax.set_aspect('equal')
+    # ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.set_xlabel('Half-life (hr)')
+    ax.set_ylabel('Fraction of mRNAs 5+ um away from nucleus\nremaining after nocodazole treatment')
+    plt.tight_layout()
+    plt.savefig('depletion_vs_half_life.png', dpi=300)
+    plt.savefig('depletion_vs_half_life.pdf', dpi=300)
+    plt.close()
+
+    # plot experimental depletion vs. predicted
+    slope, intercept, r_val, p_val, sem = linregress(fracs_theo, fracs_expt)
+    x_lin = np.linspace(0, 1, num=2, endpoint=True)
+    y_lin = slope*x_lin + intercept
+
+    print slope, intercept, r_val, p_val, sem
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(2,2)
+    ax.plot(fracs_theo, fracs_expt, 'k.')
+    for i, g in enumerate(genes_in_both):
+        ax.annotate(g, (fracs_theo[i], fracs_expt[i]))
+    # ax.plot(x_lin, y_lin, 'k-', lw=0.8)
+    ax.plot([0,1], [0,1], '--', c='#bbbbbb', lw=0.8, zorder=0)
+    ax.set_aspect('equal')
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
+    ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
+    ax.set_xlabel('Predicted fraction remaining')
+    ax.set_ylabel('Observed fraction remaining')
+    plt.tight_layout()
+    plt.savefig('depletion_vs_predicted.png', dpi=300)
+    plt.savefig('depletion_vs_predicted.pdf', dpi=300)
+    plt.close()
+
+    # statistics
+    corr, p_val = pearsonr(fracs_theo, fracs_expt)
+    print corr
+
+    out_arr = np.column_stack((genes_in_both, half_lives, fracs_theo, fracs_expt))
+    out_arr = np.insert(out_arr, 0, ['gene', 'half_life (hr)', 'predicted rem.', 'experiment rem.'], axis=0)
+
+    with open('depletion.csv', 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerows(out_arr)
+
+if p_dens is not None:
+    with open(p_dens, 'r') as dens_file:
+        reader = csv.reader(dens_file)
+        dens_data = {row[0].capitalize():[float(j) for j in row[1:]] for row in list(reader)[1:]}
+
+    print dens_data
+
+    dens_theo = []
+    dens_expt = []
+    genes_in_both = []
+
+    for name in gene_names:
+        if name in noco_data and name != 'Actn2':
+            d_theo = expo(18., 1., curve_dict[name][1])
+            dens_theo.append(d_theo)
+            dens_expt.append(dens_data[name][2]/dens_data[name][0])
+            genes_in_both.append(name)
+
+    # plot experimental depletion vs. predicted
+    slope, intercept, r_val, p_val, sem = linregress(dens_theo, dens_expt)
+    x_lin = np.linspace(0, 1, num=2, endpoint=True)
+    y_lin = slope*x_lin + intercept
+
+    print slope, intercept, r_val, p_val, sem
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(4,4)
+    ax.plot(dens_theo, dens_expt, 'k.')
+    for i, g in enumerate(genes_in_both):
+        ax.annotate(g, (dens_theo[i], dens_expt[i]))
+    # ax.plot(x_lin, y_lin, 'k-', lw=0.8)
+    ax.plot([0,1], [0,1], '--', c='#bbbbbb', lw=0.8, zorder=0)
+    ax.set_aspect('equal')
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
+    ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
+    ax.set_xlabel('Predicted cytoplasmic fraction remaining')
+    ax.set_ylabel('Observed cytoplasmic fraction remaining')
+    plt.tight_layout()
+    plt.savefig('cyto_frac_vs_predicted.png', dpi=300)
+    plt.savefig('cyto_frac_vs_predicted.pdf', dpi=300)
+    plt.close()
+
+    # statistics
+    corr, p_val = pearsonr(dens_theo, dens_expt)
+    print corr
+
+    # for name in gene_names:
+    #     if name in noco_data and name != 'Actn2':
+    #         d_theo = dens_data[name][0]*expo(18., 1., curve_dict[name][1])
+    #         dens_theo.append(d_theo)
+    #         dens_expt.append(dens_data[name][2])
+    #         genes_in_both.append(name)
+
+    # # plot experimental depletion vs. predicted
+    # slope, intercept, r_val, p_val, sem = linregress(dens_theo, dens_expt)
+    # x_lin = np.logspace(-4, 0, num=2, endpoint=True)
+    # y_lin = slope*x_lin + intercept
+
+    # print slope, intercept, r_val, p_val, sem
+
+    # fig, ax = plt.subplots()
+    # fig.set_size_inches(4,4)
+    # ax.plot(dens_theo, dens_expt, 'k.')
+    # for i, g in enumerate(genes_in_both):
+    #     ax.annotate(g, (dens_theo[i], dens_expt[i]))
+    # # ax.plot(x_lin, y_lin, 'k-', lw=0.8)
+    # ax.plot([0,1], [0,1], '--', c='#bbbbbb', lw=0.8, zorder=0)
+    # ax.set_aspect('equal')
+    # ax.set_xscale('log')
+    # ax.set_yscale('log')
+    # ax.set_xlim([1E-4,1])
+    # ax.set_ylim([1E-4,1])
+    # # ax.set_xticks([0, 0.25, 0.5, 0.75, 1])
+    # # ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
+    # ax.set_xlabel('Predicted cytoplasmic density (spots/um^3)')
+    # ax.set_ylabel('Observed cytoplasmic density (spots/um^3)')
+    # plt.tight_layout()
+    # plt.savefig('density_vs_predicted.png', dpi=300)
+    # plt.savefig('density_vs_predicted.pdf', dpi=300)
+    # plt.close()
+
+    # # statistics
+    # corr, p_val = pearsonr(dens_theo, dens_expt)
+    # print corr
